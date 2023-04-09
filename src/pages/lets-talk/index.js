0b852bb461271ad13d7a18178371a5d4b2acf037
Abhijeet Salve <48774361+abhijeet-salve@users.noreply.github.com>
2023-04-09 19:40:19 +0400

    fix margin issue

--- src/pages/lets-talk/index.js
+++ src/pages/lets-talk/index.js
@@ -9,7 +9,7 @@ const CalendlyForm = () => {
   return (
     <>
       <div
-        className="calendly-inline-widget w-100 h-screen sm:mx-24mt-24  md:-mx-24 lg:mx-32 xl:mx-80  "
+        className="calendly-inline-widget w-100 h-screen mb-100 sm:mx-24mt-24  md:-mx-24 lg:mx-32 xl:mx-80  "
         data-url={`https://calendly.com/${userName}${
           type ? `/${type}` : ""
         }?hide_landing_page_details=${showDetails}&hide_gdpr_banner=${showCookies}`}
