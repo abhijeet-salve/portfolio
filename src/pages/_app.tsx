ade2b71fdee4d32556c129a28bbb79b365c65073
Abhijeet Salve <48774361+abhijeet-salve@users.noreply.github.com>
2023-04-09 19:44:00 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class">
-        <div className=" mx-6 sm:mx-24 lg:mx-32 xl:mx-80 whitespace-pre font-Poppins">
+        <div className=" mx-6 mb-12 sm:mx-24 lg:mx-32 xl:mx-80 whitespace-pre font-Poppins">
           <div>
             <Header />
           </div>
